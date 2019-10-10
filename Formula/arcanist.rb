0d0b900eb93bc1be64cd26feada79cf47487165f
Chaitanya Pramod <me@chaitanyapramod.com>
2019-10-10 09:17:28 +0530

    Update arcanist version

--- Formula/arcanist.rb
+++ Formula/arcanist.rb
@@ -3,14 +3,14 @@ class Arcanist < Formula
   homepage "https://secure.phabricator.com/book/phabricator/article/arcanist/"
 
   stable do
-    url "https://github.com/wikimedia/arcanist/archive/release/2018-02-15/1.tar.gz"
-    sha256 "75888393e3f0ffb8a567a877b31903db8390b622f73ad501b8eb1bcafb4320fa"
-    version "201802151"
+    url "https://github.com/wikimedia/arcanist/archive/release/2019-08-22/1.tar.gz"
+    sha256 "6a5f726870224da8cf18d904bb96a1a6d8ee25d31c27ff8a0d2093884c243887"
+    version "201908221"
 
     resource "libphutil" do
-      url "https://github.com/wikimedia/phabricator-libphutil/archive/release/2018-02-15/1.tar.gz"
-      sha256 "a46f0721fa8166ed5edf24ecfe395d7d41e4728681969dded12c664c6fb2074e"
-      version "201802151"
+      url "https://github.com/wikimedia/phabricator-libphutil/archive/release/2019-08-22/1.tar.gz"
+      sha256 "5fc22b2870048b99018474212845b9c03205cab64e5b3f1c20dd72d802fc6df2"
+      version "201908221"
     end
   end
 
